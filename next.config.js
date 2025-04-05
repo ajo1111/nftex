16f2fb8f078b89301d2a9937314e3ee39cfd3306
Sky El Command Center <skyelcommandcenter@Skys-MacBook-Air.local>
2025-04-04 17:35:40 -0700

    Removed TempoLabs dev tools and updated Next.js to 14.1.3

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ if (process.env.NEXT_PUBLIC_TEMPO) {
         // NextJS 13.4.8 up to 14.1.3:
         // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
         // NextJS 14.1.3 to 14.2.11:
-        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
+        // swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
 
         // NextJS 15+ (Not yet supported, coming soon)
     }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import { TempoInit } from "@/components/tempo-init";
 import { Providers } from "@/components/ui/providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,11 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Script src="https://api.tempolabs.ai/proxy-asset?url=https://storage.googleapis.com/tempo-public-assets/error-handling.js" />
       <body className={inter.className}>
         <Providers>
           {children}
-          <TempoInit />
         </Providers>
       </body>
     </html>
